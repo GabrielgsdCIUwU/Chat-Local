8c057f031e3842f855143e7edbde43f26fc1d0d3
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2024-09-30 14:06:40 +0200

    simple web chat

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ import path from "path"
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import http from "http";
+import { Server as SocketIOServer } from "socket.io";
+//import session from "express-session"
+//import passport from "passport"
+//import FileStoreFactory from "session-file-store"
 
 import webrouter from "./router/paginas.js";
 import admin from "./router/admin.js";
+import chat from "./router/chat.js";
 
+//const FileStore = FileStoreFactory(session);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -18,16 +24,44 @@ const app = express();
 app.use(express.json());
 app.disable("x-powered-by");
 
+/* app.use(
+    session({
+        store: new FileStore({
+            path: "./sessions",
+            ttl: 86400,
+        }),
+        secret: process.env.sessionSecret,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+*/
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+const io = new SocketIOServer(server);
 
 
 app.use("/public", express.static(path.join(__dirname, "public")));
 
-app.use(webrouter)
+app.use(webrouter);
+app.use(admin);
+app.use(chat);
 
-app.use(admin)
+
+io.on("connection", (socket) => {
+    socket.on("sendmsg", (msg) => {
+        io.emit("sendmsg", msg);
+    });
+
+    socket.on("disconnect", () => {});
+});
+
+
+
 server.listen(port, () => {
     console.log("Escuchando: " + port);
-})
+});
--- router/admin.js
+++ router/admin.js
@@ -10,20 +10,68 @@ const router = express.Router();
 
 
 router.post("/login", (req, res) => {
-    const { nombre, passwd, location } = req.body
-    console.log(location)
+    const { name, passwd, location } = req.body
 
-    if(!nombre || !passwd) return res.status(400).json({message: "Debes poner usuario y contraseña!"})
+    if(!name || !passwd) return res.status(400).json({message: "Debes poner usuario y contraseña!"})
 
     if(!location) return res.status(400).json({message: "Se ha producido un error al obtener tu ip local"})
 
+    const usersFilePath = path.join(__dirname, "../public/json/users.json");
 
-    const usuario = {
-        location: location,
-        name: nombre,
-        passwd: passwd,
+    fs.readFile(usersFilePath, "utf-8", (err, data) => {
+        let currenData = [];
+        currenData = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+
+        const userIndex = currenData.findIndex((user) => user.name === name)
+        if (userIndex == -1) return res.status(400).json({message: "Usuario no encontrado"})
+        if (currenData[userIndex].name === name && currenData[userIndex].passwd === passwd) {
+            
+        }
+
+    });
+
+    return res.status(200).json({message: "Login exitoso!"})
+});
+
+router.post("/register", (req, res) => {
+    try{
+        const { location, name, passwd } = req.body
+        const usersFilePath = path.join(__dirname, "../public/json/users.json");
+        if(!name || !passwd) return res.status(400).json({message: "Debes poner usuario y contraseña!"})
+
+        fs.readFile(usersFilePath, "utf-8", (err, data) => {
+            if (err) {
+                console.log(err)
+                res.status(500).json({ message: "Error al cargar los usuarios"});
+            } 
+            let usersData = [];
+            try {
+                usersData = JSON.parse(data);
+                console.log("userdsdata: " + usersData)
+            } catch (error) {
+                return res.status(500).json({ message: "Error al pasar los usuarios"});
+            }
+            const newUser = {
+                name: name,
+                passwd: passwd,
+            }
+            usersData.push(newUser)
+            fs.writeFile(usersFilePath, JSON.stringify(usersData, null, 2), "utf-8", (err) => {
+                if(err) {
+                    console.error("Error writting users", err)
+                    return res.status(500).json({ message: "Error al escribir el usuario"})
+                }
+                
+                res.status(200).json({ message: "Usuario creado exitosamente"})
+            });
+    
+            
+        });
+    } catch (error) {
+        console.error("Unexpected error:", error);
+        res.status(500).json({ error: "Unexpected error occurred" });
     }
-    print(usuario)
+    
 });
 
 export default router;
