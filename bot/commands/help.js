94890e557a7576733dad9f79f694847d98580748
GabrielgsdCIUwU <holagabriel321@gmail.com>
2024-12-20 11:35:44 +0100

    feat(Bot commands): :sparkles: Añadir comando help

---
+++ bot/commands/help.js
@@ -0,0 +1,11 @@
+export function execute({ args, socket, io }) {
+    const timestamp = new Date().getTime();
+
+    const response = `Estos son todos los comandos:\n
+    - ping \n          **Uso:** /bot ping
+    - 8ball \n          **Uso:** /bot 8ball <pregunta>
+    `
+
+    // Emitir mensaje del bot al cliente
+    io.emit("sendmsg", { user: "🤖 Bot", message: response, timestamp });
+}
