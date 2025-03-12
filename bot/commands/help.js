4ec7f5ed35920d2aa465f94c54aebb7fd8c399f4
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-03-12 09:06:11 +0100

    feat(bot/commands): :sparkles: Añadir EmbedMessage y añadir gambling info

--- bot/commands/help.js
+++ bot/commands/help.js
@@ -1,14 +1,32 @@
+class EmbedMessage {
+    constructor() {
+        this.fields = [];
+    }
+    addField(name, value) {
+        this.fields.push({ name, value });
+        return this;
+    }
+
+    toString() {
+        return this.fields.map(field => `**${field.name}:** ${field.value}`).join("\n");
+    }
+}
+
 export function execute({ args, socket, io }) {
     const timestamp = new Date().getTime();
-
-    const response = `Estos son todos los comandos:\n
-    - ping \n          **Uso:** /bot ping
-    - 8ball \n          **Uso:** /bot 8ball (pregunta)
-    - chiste \n         **Uso:** /bot chiste
-    - horoscopo \n         **Uso:** /bot horoscopo
-    - rate \n           **Uso:** /bot rate (algo)
-    `
+    const embed = new EmbedMessage()
+    .addField("Estos son todos los comandos:", "")
+    .addField("- ping", "/bot ping")
+    .addField("- 8ball", "/bot 8ball (pregunta)")
+    .addField("- chiste", "/bot chiste")
+    .addField("- horoscopo", "/bot horoscopo")
+    .addField("- rate", "/bot rate (texto)")
+    .addField("- gambling", "Subcomandos:")
+    .addField("- gambling info", "/bot gambling info")
+    .addField("- gambling loteria", "/bot gambling loteria (cantidad)")
+    .addField("- gambling robar", "/bot gambling robar (usuario) (cantidad)")
+    .addField("- gambling banca rota", "/bot gambling bancaRota");
 
     // Emitir mensaje del bot al cliente
-    io.emit("sendmsg", { user: "🤖 Bot", message: response, timestamp });
+    io.emit("sendmsg", { user: "🤖 Bot", message: embed.toString(), timestamp });
 }
