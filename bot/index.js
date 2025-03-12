c2c8df059b6a94568c13c7352cdd950525dc1ebe
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-03-12 08:25:39 +0100

    fix(bot): :bug: Eliminar en console error en el catch y cambiar a io emit

--- bot/index.js
+++ bot/index.js
@@ -13,7 +13,6 @@ async function loadCommand(commandName) {
         const commandPath = pathToFileURL(path.join(commandsPath, `${commandName}.js`)).href;
         return await import(commandPath);
     } catch (error) {
-        console.error(`Error al cargar el comando ${commandName}:`, error);
         return null;
     }
 }
@@ -22,11 +21,10 @@ export async function handleCommand({ msg, socket, io, username }) {
     const args = msg.split(" ").slice(1); // Obtener argumentos del comando
     const commandName = args[0]; // Nombre del comando
     const command = await loadCommand(commandName);
-
     if (command && command.execute) {
         command.execute({ args: args.slice(1), socket, io, username, msg }); // Ejecutar lógica del comando
     } else {
-        socket.emit("error", { message: "Comando no encontrado" });
+        io.emit("sendmsg", { user: "🤖 Bot", message: "No existe este comando, revisa lo que has escrito: " + msg,  timestamp: new Date().getTime() });
     }
 }
 
