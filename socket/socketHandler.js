e2bfd2f94c1e1e0794f158b74f9bd6a5b2e3300e
GabrielgsdCIUwU <gsuarezdominguez@deloitte.es>
2026-06-12 14:47:53 +0200

    refactor(socket): Pasar parametro del messageRepo

--- socket/socketHandler.js
+++ socket/socketHandler.js
@@ -5,13 +5,20 @@ import registerChatEvents from "./events/chatEvents.js";
 import registerPrivateEvents from "./events/privateEvents.js";
 import registerPollEvents from "./events/pollEvents.js";
 
+import { JsonDatabaseClient } from "../backend/database/JsonDatabaseClient.js";
+import { MessageRepository } from "../backend/repositories/MessageRepository.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const dbClient = new JsonDatabaseClient(path.join(__dirname, "../public/json/messages.json"));
+const messageRepo = new MessageRepository(dbClient);
+
 
 export default function setupSockets(io, sessionMiddleware) {
     // Middleware de sesión para Sockets
     io.use((socket, next) => {
         sessionMiddleware(socket.request, {}, (err) => {
             if (err) return next(err);
-            if (socket.request.session && socket.request.session.user) {
+            if (socket.request.session?.user) {
                 next();
             } else {
                 next(new Error("Usuario no autenticado"));
@@ -26,7 +33,7 @@ export default function setupSockets(io, sessionMiddleware) {
          io.emit("userNames", Array.from(connectedUsers));
 
         registerUserEvents(io, socket, user);
-        registerChatEvents(io, socket, user);
+        registerChatEvents(io, socket, user, messageRepo);
         registerPrivateEvents(io, socket, user);
         registerPollEvents(io, socket, user);
     });       
