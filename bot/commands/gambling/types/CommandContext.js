0124f5b845079f9a6d901be3f6fe73475895429c
GabrielgsdCIUwU <gsuarezdominguez@deloitte.es>
2026-06-12 12:16:08 +0200

    feat(bot/commands): Add type CommandContext

---
+++ bot/commands/gambling/types/CommandContext.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {Object} CommandContext
+ * @property {string[]} args
+ * @property {*} io
+ * @property {string} username
+ * @property {Object} users
+ * @property {import("../../../../backend/services/GamblingService.js").GamblingService} gamblingService
+ * @property {number} timestamp
+ */
+ export {};
