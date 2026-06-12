6bd24d99df1ca7f58291e34dea3875f39fbf179b
GabrielgsdCIUwU <gsuarezdominguez@deloitte.es>
2026-06-12 12:17:06 +0200

    refactor(bot/commands): utilizar los servicios de backend

--- bot/commands/gambling/donar.js
+++ bot/commands/gambling/donar.js
@@ -1,29 +1,37 @@
+import { actualEarningsPayingDebt } from "../../utility/actualEarningsPayingDebt.js";
+
 export const params = [
     { name: "usuario", type: "user", required: true },
     { name: "dinero", type: "number", required: true }
 ]
+/**
+ * 
+ * @param {import("./types/CommandContext.js").CommandContext} context 
+ */
+export function execute(context) {
+    const targetName = context.args.slice(0, -1).join(" ");
+    const amount = Number.parseInt(context.args.at(-1));
 
-export function execute({ args, socket, io, username, currenData, userIndex, actualEarningsPayingDebt }) {
-    const timestamp = new Date().getTime()
-    let gambler = currenData[userIndex];
-    const targetName = args.slice(0, -1).join(" ");
-    const targetIndex = currenData.findIndex((user) => user.name === targetName);
+    try {
+        const { sender, target } = context.gamblingService.validateTransaction(UserRepository, context.username, targetName, amount);
 
-    if (targetIndex === -1 || targetName === username) {
-        return io.emit("sendmsg", { user: "🤖 Bot", message: `${targetName} no es válido o no existe @${username}`, timestamp });
+        sender.money -= amount;
+        target.money += actualEarningsPayingDebt(amount, target);
+
+        if (!sender.donated) sender.donated = 0;
+        sender.donated += amount;
+
+        io.emit("sendmsg", { 
+            user: "🤖 Bot", 
+            message: `${username} ha regalado ${amount}€ a ${targetName}`, 
+            timestamp 
+        });
+    } catch (error) {
+        io.emit("sendmsg", { 
+            user: "🤖 Bot", 
+            message: `${username}, ${error.message}`, 
+            timestamp 
+        });
     }
-
-    let target = currenData[targetIndex];
-    const cantidad = parseInt(args[args.length - 1]);;
-
-    if (isNaN(cantidad) || cantidad <= 0 || cantidad > gambler.money) {
-        return io.emit("sendmsg", { user: "🤖 Bot", message: `${username} tu cantidad no es válida no tienes suficiente dinero para donar.`, timestamp });
-    }
-
-
-    gambler.money -= cantidad;
-    target.money += actualEarningsPayingDebt(cantidad, target);
-    gambler.donated += cantidad;
-    return io.emit("sendmsg", { user: "🤖 Bot", message: `${username} ha regalado ${cantidad}€ a ${targetName}`, timestamp });
 
 }
