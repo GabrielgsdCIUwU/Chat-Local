fa9bb60c9746fb169a77a4c33cab6f8b22cca523
GabrielgsdCIUwU <gsuarezdominguez@deloitte.es>
2026-06-12 13:29:03 +0200

    fix(bot): Add mising .js

--- bot/index.js
+++ bot/index.js
@@ -1,6 +1,6 @@
 import { fileURLToPath} from 'node:url';
 import path from 'node:path';
-import { CommandLoader } from '../backend/core/CommandLoader';
+import { CommandLoader } from '../backend/core/CommandLoader.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
