dc11d57dc9322071e8653d3543aa12b8dd2fd585
GabrielgsdCIUwU <gsuarezdominguez@deloitte.es>
2026-06-16 13:59:23 +0200

    feat(backend/core): Añadir ChatFilterService

--- backend/core/DIContainer.js
+++ backend/core/DIContainer.js
@@ -12,6 +12,7 @@ import { UserService } from "../services/UserService.js";
 import { GamblingService } from "../services/GamblingService.js";
 import { CommandService } from "../services/CommandService.js";
 import { EmojiService } from "../services/EmojiService.js";
+import { ChatFilterService } from "../services/ChatFilterService.js";
 
 import { AuthController } from "../controllers/AuthController.js";
 import { ProfileController } from "../controllers/ProfileController.js";
@@ -30,11 +31,13 @@ class DIContainer {
         this.profileDir = path.join(__dirname, "../../resources/profiles");
         this.commandsDir = path.join(__dirname, "../../bot/commands");
         this.emojisDir = path.join(__dirname, "../../resources/emojis");
+        this.spamJsonPath = path.join(__dirname, "../../public/json/spamer.json");
 
         this.userDbClient = new JsonDatabaseClient(this.usersJsonPath);
         this.bannedDbClient = new JsonDatabaseClient(this.bannedJsonPath);
         this.gamblingDbClient = new JsonDatabaseClient(this.gamblingJsonPath);
         this.messageDbClient = new JsonDatabaseClient(this.messagesJsonPath);
+        this.spamDbClient = new JsonDatabaseClient(this.spamJsonPath);
 
         this.userRepository = new UserRepository(this.userDbClient);
         this.bannedIpRepository = new BannedIpRepository(this.bannedDbClient);
@@ -46,6 +49,7 @@ class DIContainer {
         this.gamblingService = new GamblingService(this.gamblingRepository);
         this.commandService = new CommandService(this.commandsDir);
         this.emojiService = new EmojiService(this.emojisDir);
+        this.chatFilterService = new ChatFilterService(this.spamDbClient);
 
         this.authController = new AuthController(this.authService, this.userRepository);
         this.profileController = new ProfileController(this.userService);
