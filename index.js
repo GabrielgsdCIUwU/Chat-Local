ef2e1f88ae79df177c959d01ccc92a71bc865d4d
GabrielgsdCIUwU <gsuarezdominguez@deloitte.es>
2026-06-16 11:38:45 +0200

    refactor(index): Mover parte de código del index a app

--- index.js
+++ index.js
@@ -1,62 +1,18 @@
-import express from "express";
-import path from "path";
-import dotenv from "dotenv";
-import { fileURLToPath } from "url";
-import https from "https"
-import http from "http";
+import https from "node:https";
+import http from "node:http";
+import fs from "node:fs";
+import path from "node:path";
 import { Server as SocketIOServer } from "socket.io";
-import session from "express-session";
-import passport from "passport";
-import FileStoreFactory from "session-file-store";
-import fs from "fs";
 
-import webrouter from "./router/paginas.js";
-import admin from "./router/admin.js";
-import chat from "./router/chat.js";
-import img from "./router/img.js"
-import perfil from "./router/perfil.js";
+import { app, sessionMiddleware, __dirname } from "./app.js";
 import privateRouter from "./router/private.js";
 import setupSockets from "./socket/socketHandler.js";
 
-
-const FileStore = FileStoreFactory(session);
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const envFilePath = path.join(__dirname, ".env");
-dotenv.config({ path: envFilePath });
-
-
-const app = express();
-
-app.use(express.json());
-app.disable("x-powered-by");
-
 const USE_HTTPS = process.env.USE_HTTPS === "true";
-
-const sessionMiddleware = session({
-    store: new FileStore({
-        path: "./sessions",
-        ttl: 86400,
-    }),
-    secret: process.env.sessionSecret,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        httpOnly: true,
-        secure: USE_HTTPS,
-        sameSite: USE_HTTPS ? "lax" : "strict",
-        maxAge: 1000 * 60 * 60 * 24
-    }
-});
-
-app.use(sessionMiddleware);
-app.use(passport.initialize());
-app.use(passport.session());
-
-let server;
 const HTTP_PORT = process.env.PORT || 3000;
 const HTTPS_PORT = process.env.HTTPS_PORT || 3443;
+
+let server;
 
 if (USE_HTTPS) {
     const sslOptions = {
@@ -73,7 +29,7 @@ if (USE_HTTPS) {
         });
         res.end();
     }).listen(HTTP_PORT, () => {
-        console.log(`Redireccionando`);
+        console.log(`HTTP Server redirecting to HTTPS on port ${HTTP_PORT}`);
     });
 } else {
     server = http.createServer(app);
@@ -82,17 +38,10 @@ if (USE_HTTPS) {
 const io = new SocketIOServer(server);
 setupSockets(io, sessionMiddleware);
 
-app.use("/public", express.static(path.join(__dirname, "public")));
-app.use("/resources", express.static(path.join(__dirname, "resources")));
-
-app.use(webrouter);
-app.use(admin);
-app.use(chat);
-app.use("/img", img);
-app.use("/perfil", perfil);
 app.use("/private", privateRouter(io));
 
 const ACTIVE_PORT = USE_HTTPS ? HTTPS_PORT : HTTP_PORT;
+
 server.listen(ACTIVE_PORT, () => {
-    console.log(`Escuchando: ${ACTIVE_PORT}`);
-})
+    console.log(`Servidor escuchando en el puerto: ${ACTIVE_PORT}`);
+});
