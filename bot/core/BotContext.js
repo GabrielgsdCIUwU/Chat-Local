27c724b26d46ca6c6f9b8cbdd786b2e8a2f4cef7
GabrielgsdCIUwU <gsuarezdominguez@deloitte.es>
2026-06-19 11:52:05 +0200

    feat(bot/core): Añadir guardado de mensajes al bot

--- bot/core/BotContext.js
+++ bot/core/BotContext.js
@@ -61,6 +61,15 @@ export class BotContext {
      * @param {string} message - The message content.
      */
     reply(message) {
+        const botMessage = {
+            id: crypto.randomUUID(),
+            user: "🤖 Bot",
+            message: message,
+            timestamp: Date.now()
+        };
+
+        await this.container.messageRepository.saveMessage(botMessage);
+        
         this.io.emit("sendmsg", { user: "🤖 Bot", message, timestamp: this.timestamp });
     }
 }
