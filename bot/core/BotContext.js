31943dd6d61684759472f067d1c290a85f0fc0b5
GabrielgsdCIUwU <gsuarezdominguez@deloitte.es>
2026-06-19 12:01:17 +0200

    fix(bot/core): Hacer reply async en BotContext

--- bot/core/BotContext.js
+++ bot/core/BotContext.js
@@ -60,7 +60,7 @@ export class BotContext {
      * Helper to easily reply as the Bot.
      * @param {string} message - The message content.
      */
-    reply(message) {
+    async reply(message) {
         const botMessage = {
             id: crypto.randomUUID(),
             user: "🤖 Bot",
@@ -69,7 +69,7 @@ export class BotContext {
         };
 
         await this.container.messageRepository.saveMessage(botMessage);
-        
+
         this.io.emit("sendmsg", { user: "🤖 Bot", message, timestamp: this.timestamp });
     }
 }
