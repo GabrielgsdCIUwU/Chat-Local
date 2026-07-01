832800c48f82ecc971421d8cdf7f314489b66f9a
GabrielgsdCIUwU <gsuarezdominguez@deloitte.es>
2026-07-01 10:43:36 +0200

    feat(backend/api/router): agregar ruta para asignar roles a usuarios con verificación de autenticación y administración

--- backend/api/router/admin.js
+++ backend/api/router/admin.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { container } from "../../core/DIContainer.js";
+import { isAuthenticated } from "./middlewares/isAuthenticated.js";
+import { isAdmin } from "./middlewares/isAdmin.js";
 
 const router = express.Router();
 
@@ -8,5 +10,7 @@ router.post("/login", container.authController.login);
 router.post("/register", container.authController.register);
 
 router.get("/list/users", container.authController.listUsers);
+
+router.post("/list/users/roles", isAuthenticated, isAdmin, container.authController.assignRoles);
 
 export default router;
